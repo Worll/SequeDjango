7240d9e6ad4ccd3002a3d45bb2734bb6de553cef
Worll <kvedarasarnas@gmail.com>
2020-02-24 05:02:21 +0200

    connect to spotify api

--- seque/backend/urls.py
+++ seque/backend/urls.py
@@ -3,4 +3,6 @@ from backend import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("welcome", views.welcome, name="home"),
+
 ]
--- seque/backend/views.py
+++ seque/backend/views.py
@@ -2,6 +2,38 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+import sys
+import spotipy
+import spotipy.util as util
+
+from django.conf import settings
+
+
 
 def home(request):
+
+    scope = 'user-library-read'
+    username = 'worll'
+
+    client_id=settings.SPOTIFY_CLIENT_ID
+    client_secret=settings.SPOTIFY_SECRET_KEY
+    redirect_uri=settings.SPOTIFY_REDIRECT_URI
+
+    token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        results = sp.current_user_saved_tracks()
+        for item in results['items']:
+            track = item['track']
+            print(track['name'] + ' - ' + track['artists'][0]['name'])
+    else:
+        print("Can't get token for", username)
+
     return HttpResponse("Hello, Django!")
+
+def welcome(request):
+
+
+
+    return HttpResponse("welcome")
