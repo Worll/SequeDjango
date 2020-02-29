adc6d220f394f9761b4993f9b3b7c53f67035e7d
Worll <kvedarasarnas@gmail.com>
2020-02-29 05:55:01 +0200

    add API endpoints for creating and viewing playlists

--- seque/backend/urls.py
+++ seque/backend/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from backend import views
 
 urlpatterns = [
+    path("users", views.ListUsers.as_view(), name="users"),
     path("", views.home, name="home"),
-    path("welcome", views.welcome, name="home"),
+    path("welcome", views.welcome, name="welcome"),
+    path("playlists", views.PlaylistView.as_view(), name="playlists"),
 
 ]
--- seque/backend/views.py
+++ seque/backend/views.py
@@ -7,33 +7,146 @@ import spotipy
 import spotipy.util as util
 
 from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+import random
+import string
 
+"""
+    // TODO - use dry principle and remove repetitive code
+"""
+
+class ListUsers(APIView):
+    """
+    View to list all users in the system.
+
+    // TODO - add authorization/authentication
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        """
+        API endpoint that allows users to be viewed or edited.
+        """
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
+
+    def post(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        """
+        API endpoint that allows users to be viewed or edited.
+        """
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
+
+class PlaylistView(APIView):
+    """
+    View to list all users in the system.
+
+    // TODO - add authorization/authentication
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+
+    def get(self, request, format=None):
+        """
+        Return a list of all playlists.
+
+        // TODO - delete this function, it's only for testing purposes and should not be allowed for users
+        """
+        """
+        API endpoint that allows users to get all playlist.
+        """
+        
+        scope = 'playlist-modify-public'
+        username = 'worll'
+
+        client_id=settings.SPOTIFY_CLIENT_ID
+        client_secret=settings.SPOTIFY_SECRET_KEY
+        redirect_uri=settings.SPOTIFY_REDIRECT_URI
+        token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+
+        if token:
+            sp = spotipy.Spotify(auth=token)
+
+            playlists = sp.user_playlists(username)
+            return Response(playlists)
+
+        else:
+            message = "Problem with authorization flow to spotify api"
+            return Response(message)
+        return HttpResponse("welcome")
+
+
+    def post(self, request, format=None):
+        """
+        Return newly created playlist url.
+        """
+        """
+        API endpoint that allows users to create new playlists.
+        """
+        scope = 'playlist-modify-public'
+        username = 'worll'
+
+        client_id=settings.SPOTIFY_CLIENT_ID
+        client_secret=settings.SPOTIFY_SECRET_KEY
+        redirect_uri=settings.SPOTIFY_REDIRECT_URI
+        token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+
+        if token:
+            sp = spotipy.Spotify(auth=token)
+            playlistURL = sp.user_playlist_create(username, "test")
+            return Response(playlistURL['external_urls']['spotify'])            
+        else:
+            message = "Problem with authorization flow to spotify api"
+            return Response(message)
+        return HttpResponse("welcome")
 
 
 def home(request):
+    return HttpResponse("Hello, Django!")
 
-    scope = 'user-library-read'
+def playlists(request):
+
+    scope = 'playlist-modify-public'
     username = 'worll'
 
     client_id=settings.SPOTIFY_CLIENT_ID
     client_secret=settings.SPOTIFY_SECRET_KEY
     redirect_uri=settings.SPOTIFY_REDIRECT_URI
-
     token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
 
     if token:
         sp = spotipy.Spotify(auth=token)
-        results = sp.current_user_saved_tracks()
-        for item in results['items']:
-            track = item['track']
-            print(track['name'] + ' - ' + track['artists'][0]['name'])
+        playlists = sp.user_playlists(username)
+        for playlist in playlists['items']:
+            if playlist['owner']['id'] == username:
+                print()
+                print(playlist['name'])
+                print ('  total tracks', playlist['tracks']['total'])
+                results = sp.playlist(playlist['id'],
+                    fields="tracks,next")
+                tracks = results['tracks']
+                while tracks['next']:
+                    tracks = sp.next(tracks)
     else:
         print("Can't get token for", username)
 
-    return HttpResponse("Hello, Django!")
-
 def welcome(request):
-
-
-
     return HttpResponse("welcome")
+
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
