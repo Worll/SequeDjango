ecfce38e00bd16cc44755d78aa0371336231ce55
Worll <kvedarasarnas@gmail.com>
2020-05-15 22:11:19 +0300

    add socket io immplementation

--- seque/wsgi.py
+++ seque/wsgi.py
@@ -6,11 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
+import socketio
 
 import os
+from backend.socketView import sio
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seque.settings')
 
-application = get_wsgi_application()
+django_app = get_wsgi_application()
+application = socketio.WSGIApp(sio, django_app)
